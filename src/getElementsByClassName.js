db84589983982160e036c378b0a4be733491227a
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-03-23 18:22:41 -0700

    Update getElementsByClassName.js to Pass Updated Mocha Test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
+
   // your code here
+  var elementArray = [];
+
+  node = node || document.body;
+
+  if (node.classList.contains(className)) {
+    elementArray.push(node);
+  }
+
+
+    for (var i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].nodeType===1){
+      elementArray = elementArray.concat(getElementsByClassName(className, node.childNodes[i]));
+     }
+  }
+return elementArray;
+
 };
